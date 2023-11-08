715376c2c4d7348a1802d628b7da5e17d0725568
AhmadAbdElhameed <Abdelhameed9@yahoo.com>
2023-11-09 01:50:14 +0200

    first comiit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import {createApp} from 'vue';
+import Login from './login/login.vue';
+
+const app = createApp({});
+app.component('login-component',Login);
+app.mount('#app');
